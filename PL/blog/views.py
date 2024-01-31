4eacffae09bc531cc8b26ffc95565a8b3f543b0b
user.email <soniccrush@naver.com>
2024-02-01 01:01:11 +0900

    파이로그 14.4 부터 재개

---
+++ PL/blog/views.py
@@ -0,0 +1,39 @@
+from django.shortcuts import render, redirect
+from blog.models import Post
+
+def post_list(request):
+    posts = Post.objects.all()
+    context = {
+        "posts": posts,
+    }
+    
+    return render(request, "post_list.html", context)
+
+def post_detail(request, post_id):
+    post = Post.objects.get (id=post_id)
+    print(post)
+    
+    
+    context = {
+        "post": post,
+    }
+    return render (request, "post_detail.html", context)
+
+def post_add(request):
+  if request.method == "POST":
+    print("method POST")
+    title = request.POST["title"]
+    content = request.POST["content"]
+    post = Post.objects.create(
+        title=title,
+        content=content,
+    )
+    return redirect(f"/posts/{post.id}/")
+    print(title)
+    print(content)
+    return render (frequest, "post_add.html")
+    
+  else:
+    print("method GET")
+    return render(request, "post_add.html")
+  
